d731ce0491e840f2edb2756bd2e519f606439101
Ernesto Pedrosa Alonso <ernestop891123@gmail.com>
2024-03-17 10:44:46 +0100

    refactor: fix some keybindings

--- nvim/lua/ernesto/plugins/harpoon.lua
+++ nvim/lua/ernesto/plugins/harpoon.lua
@@ -16,15 +16,15 @@ return {
     keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
     keymap.set(
       "n",
-      "<leader>hv",
-      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
-      { desc = "Go to next harpoon mark" }
-    )
-    keymap.set(
-      "n",
       "<leader>hp",
       "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
       { desc = "Go to previous harpoon mark" }
+    )
+    keymap.set(
+      "n",
+      "<leader>hq",
+      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
+      { desc = "Harpoon quick menu" }
     )
   end,
 }
