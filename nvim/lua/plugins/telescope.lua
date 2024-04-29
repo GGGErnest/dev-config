f2434b89aee78aa6da8584333cb1b8c293dbb152
Ernesto Pedrosa Alonso <ernestop891123@gmail.com>
2024-04-29 09:55:51 +0200

    feat: disable flash and update some other plugins settings
    - unbind some key mappings from neo-tree
    - update telescope to use file-browser plugin
    - add lsp-lens plugin to get code references in inlayhints

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,175 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    -- disable the keymap to grep files
-    { "<leader>/", false },
-    -- change a keymap
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    -- add a keymap to browse plugin files
+  "telescope.nvim",
+  dependencies = {
     {
-      "<leader>pp",
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+    },
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
+  keys = {
+    {
+      "<leader>,",
+      "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
+      desc = "Switch Buffer",
+    },
+    { "<leader>/", LazyVim.telescope("live_grep"), desc = "Grep (Root Dir)" },
+    { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+    { "<leader><space>", LazyVim.telescope("files"), desc = "Find Files (Root Dir)" },
+    -- find
+    { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+    { "<leader>fc", LazyVim.telescope.config_files(), desc = "Find Config File" },
+    { "<leader>fF", false },
+    { "<leader>ff", LazyVim.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+    { "<leader>fg", false },
+    { "<leader>fr", false },
+    { "<leader>fR", false },
+    -- git
+    { "<leader>gc", false },
+    { "<leader>gs", false },
+    -- search
+    { '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
+    { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+    { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+    { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+    { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+    { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
+    { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
+    { "<leader>sg", false },
+    { "<leader>sG", LazyVim.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" },
+    { "<leader>sh", false },
+    { "<leader>sH", false },
+    { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+    { "<leader>so", false },
+    { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+    { "<leader>sw", LazyVim.telescope("grep_string", { word_match = "-w" }), desc = "Word (Root Dir)" },
+
+    { "<leader>sW", LazyVim.telescope("grep_string", { cwd = false, word_match = "-w" }), desc = "Word (cwd)" },
+    { "<leader>sw", LazyVim.telescope("grep_string"), mode = "v", desc = "Selection (Root Dir)" },
+    { "<leader>sW", LazyVim.telescope("grep_string", { cwd = false }), mode = "v", desc = "Selection (cwd)" },
+
+    { "<leader>uC", LazyVim.telescope("colorscheme", { enable_preview = true }), desc = "Colorscheme with Preview" },
+    {
+      "<leader>ss",
+      function()
+        require("telescope.builtin").lsp_document_symbols({
+          symbols = require("lazyvim.config").get_kind_filter(),
+        })
+      end,
+      desc = "Goto Symbol",
+    },
+    {
+      "<leader>sS",
+      function()
+        require("telescope.builtin").lsp_dynamic_workspace_symbols({
+          symbols = require("lazyvim.config").get_kind_filter(),
+        })
+      end,
+      desc = "Goto Symbol (Workspace)",
+    },
+    {
+      ";;",
+      function()
+        local builtin = require("telescope.builtin")
+        builtin.resume()
+      end,
+      desc = "Resume the previous telescope picker",
+    },
+    {
+      ";e",
+      function()
+        local builtin = require("telescope.builtin")
+        builtin.diagnostics()
+      end,
+      desc = "Lists Diagnostics for all open buffers or a specific buffer",
+    },
+    {
+      ";s",
+      function()
+        local builtin = require("telescope.builtin")
+        builtin.treesitter()
+      end,
+      desc = "Lists Function names, variables, from Treesitter",
+    },
+    {
+      "<leader>e",
       function()
         local telescope = require("telescope")
-        -- load yank history
-        telescope.load_extension("yank_history")
+
+        local function telescope_buffer_dir()
+          return vim.fn.expand("%:p:h")
+        end
+
+        telescope.extensions.file_browser.file_browser({
+          path = "%:p:h",
+          cwd = telescope_buffer_dir(),
+          respect_gitignore = false,
+          hidden = true,
+          grouped = true,
+          previewer = false,
+          initial_mode = "normal",
+          layout_config = { height = 40 },
+        })
       end,
-      desc = "Yank History",
+      desc = "Open File Browser with the path of the current buffer",
     },
   },
+  config = function(_, opts)
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local fb_actions = require("telescope").extensions.file_browser.actions
+
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+      wrap_results = true,
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      mappings = {
+        n = {},
+      },
+    })
+    opts.pickers = {
+      diagnostics = {
+        theme = "ivy",
+        initial_mode = "normal",
+        layout_config = {
+          preview_cutoff = 9999,
+        },
+      },
+    }
+    opts.extensions = {
+      file_browser = {
+        theme = "dropdown",
+        -- disables netrw and use telescope-file-browser in its place
+        hijack_netrw = true,
+        mappings = {
+          -- your custom insert mode mappings
+          ["n"] = {
+            -- your custom normal mode mappings
+            ["N"] = fb_actions.create,
+            ["h"] = fb_actions.goto_parent_dir,
+            ["/"] = function()
+              vim.cmd("startinsert")
+            end,
+            ["<C-u>"] = function(prompt_bufnr)
+              for i = 1, 10 do
+                actions.move_selection_previous(prompt_bufnr)
+              end
+            end,
+            ["<C-d>"] = function(prompt_bufnr)
+              for i = 1, 10 do
+                actions.move_selection_next(prompt_bufnr)
+              end
+            end,
+            ["<PageUp>"] = actions.preview_scrolling_up,
+            ["<PageDown>"] = actions.preview_scrolling_down,
+          },
+        },
+      },
+    }
+    telescope.setup(opts)
+    require("telescope").load_extension("fzf")
+    require("telescope").load_extension("file_browser")
+  end,
 }
