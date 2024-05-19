9a4d06ba41258f26552478ad24f03edce7b916cb
Ernesto Pedrosa Alonso <ernestop891123@gmail.com>
2024-05-19 09:20:44 +0200

    feat: add more plugins
    - add mini-animate plugin
    - add dap for debugging
    - update lazyvim to latest release
    - update vim to version 0.10

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,67 +8,9 @@ return {
     "nvim-telescope/telescope-file-browser.nvim",
   },
   keys = {
+    { "<leader><space>", false },
     {
-      "<leader>,",
-      "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
-      desc = "Switch Buffer",
-    },
-    { "<leader>/", LazyVim.telescope("live_grep"), desc = "Grep (Root Dir)" },
-    { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-    { "<leader><space>", LazyVim.telescope("files"), desc = "Find Files (Root Dir)" },
-    -- find
-    { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
-    { "<leader>fc", LazyVim.telescope.config_files(), desc = "Find Config File" },
-    { "<leader>fF", false },
-    { "<leader>ff", LazyVim.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
-    { "<leader>fg", false },
-    { "<leader>fr", false },
-    { "<leader>fR", false },
-    -- git
-    { "<leader>gc", false },
-    { "<leader>gs", false },
-    -- search
-    { '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
-    { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
-    { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
-    { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-    { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-    { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
-    { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
-    { "<leader>sg", false },
-    { "<leader>sG", LazyVim.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" },
-    { "<leader>sh", false },
-    { "<leader>sH", false },
-    { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-    { "<leader>so", false },
-    { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
-    { "<leader>sw", LazyVim.telescope("grep_string", { word_match = "-w" }), desc = "Word (Root Dir)" },
-
-    { "<leader>sW", LazyVim.telescope("grep_string", { cwd = false, word_match = "-w" }), desc = "Word (cwd)" },
-    { "<leader>sw", LazyVim.telescope("grep_string"), mode = "v", desc = "Selection (Root Dir)" },
-    { "<leader>sW", LazyVim.telescope("grep_string", { cwd = false }), mode = "v", desc = "Selection (cwd)" },
-
-    { "<leader>uC", LazyVim.telescope("colorscheme", { enable_preview = true }), desc = "Colorscheme with Preview" },
-    {
-      "<leader>ss",
-      function()
-        require("telescope.builtin").lsp_document_symbols({
-          symbols = require("lazyvim.config").get_kind_filter(),
-        })
-      end,
-      desc = "Goto Symbol",
-    },
-    {
-      "<leader>sS",
-      function()
-        require("telescope.builtin").lsp_dynamic_workspace_symbols({
-          symbols = require("lazyvim.config").get_kind_filter(),
-        })
-      end,
-      desc = "Goto Symbol (Workspace)",
-    },
-    {
-      ";;",
+      "<leader><space>",
       function()
         local builtin = require("telescope.builtin")
         builtin.resume()
@@ -76,15 +18,7 @@ return {
       desc = "Resume the previous telescope picker",
     },
     {
-      ";e",
-      function()
-        local builtin = require("telescope.builtin")
-        builtin.diagnostics()
-      end,
-      desc = "Lists Diagnostics for all open buffers or a specific buffer",
-    },
-    {
-      ";s",
+      "<leader>ct",
       function()
         local builtin = require("telescope.builtin")
         builtin.treesitter()
@@ -147,21 +81,7 @@ return {
           -- your custom insert mode mappings
           ["n"] = {
             -- your custom normal mode mappings
-            ["N"] = fb_actions.create,
-            ["h"] = fb_actions.goto_parent_dir,
-            ["/"] = function()
-              vim.cmd("startinsert")
-            end,
-            ["<C-u>"] = function(prompt_bufnr)
-              for i = 1, 10 do
-                actions.move_selection_previous(prompt_bufnr)
-              end
-            end,
-            ["<C-d>"] = function(prompt_bufnr)
-              for i = 1, 10 do
-                actions.move_selection_next(prompt_bufnr)
-              end
-            end,
+            ["a"] = fb_actions.create,
             ["<PageUp>"] = actions.preview_scrolling_up,
             ["<PageDown>"] = actions.preview_scrolling_down,
           },
