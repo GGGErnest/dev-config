2167d66e41f89399d05c0fae74fffb6fc91961f5
Ernesto Pedrosa Alonso <pedrosa.e@protonmail.com>
2024-09-07 17:12:00 +0200

    feat: disable "termsync" of nvim that causes some sluginess when running in wezterm

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,5 @@ language = en_US
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 vim.g.lazyvim_prettier_needs_config = false
+--  This is a workaround to work with nvim in mux mode in Wezterm. Enabling this causes sluggishness in nwovim
+vim.opt.termsync = false
